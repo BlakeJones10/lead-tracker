906f47c3393942a035e2da1fd77752a1ea50f69a
Blake Jones <blakejones10@icloud.com>
2025-03-03 16:53:48 -0500

    Turn extension into PWA

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-database.js";
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
+const firebaseConfig = {
+   databaseURL: "https://leads-tracker-app-5af74-default-rtdb.firebaseio.com/"
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const referneceInDB = ref(database, "leads");
+
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
 
 function render(leads) {
-    let listItems = ""
+    let listItems = "";
     for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
@@ -28,20 +23,27 @@ function render(leads) {
                     ${leads[i]}
                 </a>
             </li>
-        `
+        `;
     }
-    ulEl.innerHTML = listItems
+    ulEl.innerHTML = listItems;
 }
 
+
+onValue(referneceInDB, function(snapshot) {
+    const snapshotExists = snapshot.exists();
+    if (snapshotExists) {
+        const snapshotValue = snapshot.val();
+        const leads = Object.values(snapshotValue);
+        render(leads);
+    }
+});
+
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
+    remove(referneceInDB);
+    ulEl.innerHTML = "";
+});
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
-})
+    push(referneceInDB, inputEl.value);
+    inputEl.value = "";
+});
